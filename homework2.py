a11ab2f0c93880d7dd134c3751b2b51eb0eb0de7
Anton Kozhemyakin <hoppvo2020@mail.ru>
2024-08-14 15:30:03 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+quantity_homework = 12
+hours = 1.5
+name_course = 'Python'
+time_for_work = hours / quantity_homework
+print('Курс: ', name_course, ', всего задач:', quantity_homework, ', затрачено часов: ', hours, ', среднее время выполнения', time_for_work, 'часа.')
---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+name = "Антон"
+print("Name:", name)
+age = 24
+print("Age:", age)
+new_age = 1
+age = 24 + new_age
+print("New_age:", age)
+is_student = True
+print("Is_student:", is_student)
