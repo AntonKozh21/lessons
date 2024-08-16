517c622f3d239d41a2e76dd71e98c1344094f972
Anton Kozhemyakin <hoppvo2020@mail.ru>
2024-08-16 17:59:42 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = 21, True, 'Синхрофазатрон'
+print(immutable_var)
+# immutable_var[1] = 32
+# print(immutable_var)
+# в отличие от списка, элементы в кортеже изменить нелья
+mutable_list = [32, False, 'Карцер']
+mutable_list[2] = 'Свобода'
+print(mutable_list)
