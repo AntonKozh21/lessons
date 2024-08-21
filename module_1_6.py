4a897083042ab421d461d63bba033e8832ab2fe6
Anton Kozhemyakin <hoppvo2020@mail.ru>
2024-08-21 13:34:03 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,15 @@
+my_dict = {'Anton': 2000, 'Kristina': 1998, 'Evgeniy': 1999}
+print(my_dict)
+print(my_dict['Anton'])
+print(my_dict.get('Kirill'))
+my_dict.update({'Viktor': 1995,
+                'Irina': 1996})
+del my_dict['Evgeniy']
+print(my_dict)
+# работа с множествами
+my_set = {21, 21, 21, True, 'Fox', True, 21}
+print(my_set)
+my_set.update({'Cow',
+               (3.14, 3, 777)})
+my_set.discard('Fox')
+print(my_set)
